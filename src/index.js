dcf04070cc41d591ef1c8d97b176ada0b82ad0c0
mmccarthy64 <mmccarthy64@hotmail.com>
2020-11-30 13:17:32 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+
+  const newTaskUl = document.getElementById("list");
+
+  newTaskForm.addEventListener("submit", createNewTask);
 });
+
+  const createNewTask = event => {
+    event.preventDefault();
+
+    const newTaskDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerHTML = newTaskDescription.value;
+
+    appendNewTask(newTask);
+    event.target.reset();
+  }
+
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+  };
